c5dec8a7ff81756a91f0e000df39189d440440e5
JoseAle2610 <jalesgervazzi@gmail.com>
2021-12-16 10:07:23 -0400

    full español

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -47,19 +47,19 @@ const StyledDetail = styled(Detail)`
 
 const About = () => (
   <Container>
-    <Title>About</Title>
+    <Title>Acerca de mi</Title>
     <p style={{marginBottom: '20px'}}>Técnico Superior en Informática, apasionado por la tecnología y el aprendizaje, aspirando a un puesto de entrada que permita desarrollar y poner a disposición mis habilidades y desenvolverse en el ámbito laboral.</p>
     <Row>
       <Col size={1}>
-        <h4 style={{marginBottom: '15px'}}>Personal Details</h4>
-        <StyledDetail name='Cumpleaños' value='26-10-2001'/>
+        <h4 style={{marginBottom: '15px'}}>Detalles personales</h4>
+        <StyledDetail name='Nacimiento' value='26-10-2001'/>
         <StyledDetail name='Telefono' value='0414-5110735'/>
         <StyledDetail name='Email' value='jalesgervazzi@gmil.com'/>
         <StyledDetail name='Direccion' value='Carrera 19 entre calles 20 y 21'/>
         <StyledDetail name='Pagina Web' value='jsuarez.online'/>
       </Col>
       <Col size={1}>
-        <h4>My Interests</h4>
+        <h4>Mis intereses</h4>
         <StyledInterest icon='github' text='github' />
         <StyledInterest icon='file-earmark-music' text='Music' />
         <StyledInterest icon='pencil' text='Art' />
